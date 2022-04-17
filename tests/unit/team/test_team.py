1e1fafd5d7b8e148eb2cebe1bdedfd7509d28f1d
japinol <japinol@outlook.com>
2022-04-18 00:22:30 +0200

    [IMP] Add and improve some tests for these modules: team.

--- tests/unit/team/test_team.py
+++ tests/unit/team/test_team.py
@@ -1,25 +1,38 @@
 from modules.team.team import calc_team
 
+TEAM_NAME_ARBITRARY = 'Team_01'
+ERROR_RES_NOT_ENOUGH_CHARACTERS = {TEAM_NAME_ARBITRARY: ('Error', 'Not enough Characters to generate this team')}
+
 
 def test_calc_team_1_member(team_names_resource, random_choice_names_mock):
-    result = calc_team(team_name='Team_01', names=team_names_resource, names_sel=[], n_members=1)
-    result_expected = {'Team_01': ['Rei Ayanami']}
+    result = calc_team(team_name=TEAM_NAME_ARBITRARY, names=team_names_resource, names_sel=[], n_members=1)
+    result_expected = {TEAM_NAME_ARBITRARY: ['Rei Ayanami']}
     assert result == result_expected
 
 
 def test_calc_team_3_members(team_names_resource, random_choice_names_mock):
-    result = calc_team(team_name='Team_01', names=team_names_resource, names_sel=[], n_members=3)
-    result_expected = {'Team_01': ['Rei Ayanami', 'Son Goku', 'Monokuma']}
+    result = calc_team(team_name=TEAM_NAME_ARBITRARY, names=team_names_resource, names_sel=[], n_members=3)
+    result_expected = {TEAM_NAME_ARBITRARY: ['Rei Ayanami', 'Son Goku', 'Monokuma']}
     assert result == result_expected
 
 
 def test_calc_team_no_members(team_names_resource, random_choice_names_mock):
-    result = calc_team(team_name='Team_01', names=team_names_resource, names_sel=[], n_members=0)
-    result_expected = {'Team_01': []}
+    result = calc_team(team_name=TEAM_NAME_ARBITRARY, names=team_names_resource, names_sel=[], n_members=0)
+    result_expected = {TEAM_NAME_ARBITRARY: []}
     assert result == result_expected
 
 
-def test_calc_team_not_enough_names(random_choice_names_mock):
-    result = calc_team(team_name='Team_01', names=['Name1', 'Name2'], names_sel=[], n_members=3)
-    result_expected = {'Team_01': ('Error', 'Not enough Characters to generate this team')}
+def test_calc_team_not_enough_names():
+    result = calc_team(team_name=TEAM_NAME_ARBITRARY, names=['Name1', 'Name2'], names_sel=[], n_members=3)
+    result_expected = ERROR_RES_NOT_ENOUGH_CHARACTERS
+    assert result == result_expected
+
+
+def test_calc_team_not_enough_names_already_sel():
+    """Test selecting 3 names from a list of 3 names, one of them already selected."""
+    result = calc_team(team_name=TEAM_NAME_ARBITRARY,
+                       names=['Name1', 'Name2', 'Name3'],
+                       names_sel=['Name2'],
+                       n_members=3)
+    result_expected = ERROR_RES_NOT_ENOUGH_CHARACTERS
     assert result == result_expected
